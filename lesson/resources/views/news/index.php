1ef8dd9e5845fdc66466d3d63b483e20df2e3ce1
andrew-1998 <andrey.poloskin2015@gmail.com>
2021-06-10 19:15:00 +0500

    Третий урок Шаблонизатор

--- lesson/resources/views/news/index.php
+++
@@ -1,6 +0,0 @@
-<?php  
-
-foreach ($newsList as $key=>$news) {
-	++$key; //Это просто индекс массива, начинающегося с нуля, поэтому его надо инкрементировать
-    echo "$news &nbsp; <a href='".route('news.show', ['id'=>$key])."'> К новости</a> <br>";
-}
